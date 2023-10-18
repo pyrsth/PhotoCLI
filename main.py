f16845fb3a2900965bfab86ce8257da06b0b80b3
Taha Mokhtary <120749696+tahamokhtary@users.noreply.github.com>
2023-10-18 20:21:52 +0330

    Comment codes and add error of librarys not installed

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from PIL import Image
-from rembg import remove
-import argparse
+try : 
+    from PIL import Image
+except : 
+    print("ERROR : PYTHON MODULE PIL NOT INSTALLED")
+    import sys
+    sys.exit()
+try : 
+    from rembg import remove
+except : 
+    print("ERROR : PYTHON MODULE rembg NOT INSTALLED")
+try :    
+    import argparse
+except : 
+    print("ERROR : PYTHON MODULE argparse NOT INSTALLED")
 
 def removebg(address,output):
     input_path=address #input address image
@@ -17,17 +28,17 @@ def rmexif(address):
     image_without_exif.putdata(data)
     image_without_exif.save('image_file_without_exif.jpeg')
     print("Photo exif is removed")
+## 4 lines about Create flags for programme feature
 parser=argparse.ArgumentParser(description="Welcome to PhotoCLI !  PhotoCLI 0.01 alpha  Taha Mokhtary HashemAbad <taha490mokh@gmail.com> GPLv3")
 parser.add_argument('-e',help="Remove exif your photo.",action='store_true',default=None)
 parser.add_argument('-b',help="Remove background for your photo.",action='store_true',default=None)
 parser.add_argument('--version', action='version', version='PhotoCLI 0.01 alpha,  Taha Mokhtary HashemAbad <taha490mokh@gmail.com> GPLv3')
 ns = parser.parse_args()
-if ns.e :
+if ns.e :#two lines of remove exif
     exphotoaddress=input("Enter a photo address: ")
     rmexif(exphotoaddress)
  
-if ns.b :
-    
+if ns.b :#3 lines of remove background
     adress_inp=input("Enter a photo address: ")
     removebg(adress_inp,f'{adress_inp}_removed_bakground.png')
     print("Photo background is removed ")
