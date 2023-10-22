ec4b44a14281f760166693731d2aad386825d191
Taha Mokhtary <120749696+tahamokhtary@users.noreply.github.com>
2023-10-22 21:43:01 +0330

    add set quality
    It's beta.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-try : 
+ try : 
     from PIL import Image
 except : 
     print("ERROR : PYTHON MODULE PIL NOT INSTALLED")
@@ -28,11 +28,15 @@ def rmexif(address):
     image_without_exif.putdata(data)
     image_without_exif.save('image_file_without_exif.jpeg')
     print("Photo exif is removed")
+def setquality(quality,image_path):
+    image_file = Image.open(image_path)
+    image_file.save("f{image_path}_changed_quality.png", quality=quality) 
 ## 4 lines about Create flags for programme feature
 parser=argparse.ArgumentParser(description="Welcome to PhotoCLI !  PhotoCLI 0.01 alpha  Taha Mokhtary HashemAbad <taha490mokh@gmail.com> GPLv3")
 parser.add_argument('-e',help="Remove exif your photo.",action='store_true',default=None)
 parser.add_argument('-b',help="Remove background for your photo.",action='store_true',default=None)
 parser.add_argument('--version', action='version', version='PhotoCLI 0.01 alpha,  Taha Mokhtary HashemAbad <taha490mokh@gmail.com> GPLv3')
+parser.add_argument('-sq',action='store_ture',help="Set quality for your photo.",default=None)
 ns = parser.parse_args()
 if ns.e :#two lines of remove exif
     exphotoaddress=input("Enter a photo address: ")
@@ -42,3 +46,7 @@ if ns.b :#3 lines of remove background
     adress_inp=input("Enter a photo address: ")
     removebg(adress_inp,f'{adress_inp}_removed_bakground.png')
     print("Photo background is removed ")
+if ns.sq :
+    imagepath=input("Enter address your: ")
+    quality=input("Enter quality(1-100): ")
+    setquality(imagepath,quality)
